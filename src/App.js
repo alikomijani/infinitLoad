cf0d975d5c575af88d9871e4fede82727517cbe8
komij <akpa125@gmail.com>
2020-09-17 16:05:41 +0430

    first initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { Container, Row, Col, Card, CardImg, CardBody, CardTitle, Button } from 'reactstrap'
+import debounce from "lodash.debounce";
 
+import './App.css';
+import axios from 'axios'
 function App() {
+  const [pics, setPics] = useState([])
+
+  const [loading, setLoding] = useState(false)
+  const [page, setPage] = useState(1)
+
+  const getPic = () => {
+    axios.get(`https://jsonplaceholder.typicode.com/photos?_page=${page}&_limit=6`).then(res => {
+      setPage(page + 1)
+      setPics([...pics, ...res.data])
+    }
+    )
+  }
+
+  useEffect(() => {
+    if (loading)
+      {getPic()}
+  }, [loading])
+
+
+  const listener = debounce((e) => {
+    if (
+      window.innerHeight + document.documentElement.scrollTop + 10
+      >= document.documentElement.offsetHeight
+    ) {
+      setLoding(true)
+    } else {
+      setLoding(false)
+    }
+
+  }, 100)
+  useEffect(() => {
+    getPic()
+    window.addEventListener("scroll", listener)
+    return () => {
+      window.removeEventListener("scroll", listener);
+    };
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container on className='mx-5'>
+      {page}
+      <Row>
+        {pics.map(pic => (
+          <Col className='mt-3' key={pic.id} xs={12} md={6} lg={4}>
+            <Card>
+              <CardImg top width="100%" src={pic.url} alt={pic.title} />
+              <CardBody>
+                <CardTitle>{pic.title} </CardTitle>
+              </CardBody>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </Container>
+
+
   );
 }
 
